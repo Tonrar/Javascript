65fdb3608df082fcae2391a5d286269a2a15989c
Tonrar <alkaliumriversen@gmail.com>
2025-06-18 16:11:48 +0200

    Update 03-beginner-exercises.js

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -4,10 +4,16 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 */
 
 // 1. Escribe un comentario en una línea
+// Este es un comentario en una linea
 
 // 2. Escribe un comentario en varias líneas
+// Este es un comentario
+// en varias lineas
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let number = 5
+let string = "Hola!"
+
 
 // 4. Imprime por consola el valor de todas las variables
 
